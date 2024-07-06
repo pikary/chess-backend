d44fdc98c95edde1693bbe300151f73048668268
nurlan <baitassovnurlan1507@gmail.com>
2024-07-07 01:47:19 +0500

    learning flask

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,5 @@
 import os
 from flask import Flask
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
 
 def create_app(test_config=None):
     app = Flask(__name__,instance_relative_config=True)
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,18 +1,23 @@
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, current_user
 import functools
-import bcrypt
+from flask_bcrypt import Bcrypt
 from flask import (
-    Blueprint, request, jsonify
+    Blueprint, request, jsonify, make_response, current_app
 )
 from flaskr.db import get_db
 from datetime import timedelta
+import logging
+
 
 jwt = JWTManager()
+bcrypt = Bcrypt(app=current_app)
+
 def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.generate_password_hash(password).decode('utf-8')
+
 
 def verify_password(stored_password, provided_password):
-    return bcrypt.checkpw(provided_password.encode('utf-8'), stored_password.encode('utf-8'))
+    return bcrypt.check_password_hash(stored_password, provided_password)
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -32,12 +37,19 @@ def register():
         return jsonify({"error": "Username already taken"}), 409
 
     hashed_password = hash_password(password)
-    cursor.execute("INSERT INTO user (username,email, password) VALUES (?, ?,?)", (username,email, hashed_password))
+    cursor.execute("INSERT INTO user (username,email, password) VALUES (?, ?,?)", (username, email, hashed_password))
     db.commit()
     user_id = cursor.lastrowid
-    access_token = create_access_token(identity=user_id,expires_delta=timedelta(minutes=60))
+    access_token = create_access_token(identity=user_id, expires_delta=timedelta(minutes=60))
 
-    return jsonify(username = username, email=email, access_token=access_token, message= "User created successfully"), 201
+    return jsonify({
+        "user": {
+            "username": username,
+            "email": email,
+            "access_token": access_token
+        },
+        "message": "User created successfully"
+    }), 201
 
 
 @bp.route('/login', methods=['POST'])
@@ -51,6 +63,8 @@ def login():
     cursor = db.cursor()
     cursor.execute("SELECT id, password FROM user WHERE username = ?", (username,))
     user = cursor.fetchone()
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug(user['password'])
     if user is None:
         return jsonify({"error": "Incorrect username"}), 401
 
@@ -58,9 +72,11 @@ def login():
         return jsonify({"error": "Incorrect password"}), 401
 
     print(cursor)
+    access_token = create_access_token(identity=cursor.lastrowid, expires_delta=timedelta(minutes=60))
+    response = make_response(jsonify({"message": "Login successful", "user": user}), 200)
+    response.headers['Authorization'] = f'Bearer {access_token}'
+    return response
 
-    access_token = create_access_token(identity=cursor.lastrowid,expires_delta=timedelta(minutes=60))
-    return jsonify(access_token=access_token), 200
 
 @bp.route('/getMe', methods=['GET'])
 @jwt_required()
@@ -77,6 +93,7 @@ def user_identity_lookup(user):
     print(user)
     return user
 
+
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
@@ -85,6 +102,6 @@ def user_lookup_callback(_jwt_header, jwt_data):
     user = cursor.fetchone()
     return user
 
+
 def init_jwt(app):
     jwt.init_app(app)
-
